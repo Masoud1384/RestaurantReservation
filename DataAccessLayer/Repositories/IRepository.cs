e291084f47f957bcdfb74687ba7e7578971cc731
masoud <mwsoush.84@gmail.com>
2023-09-19 11:01:01 -0700

    Generic IRepository has been added nad implemented

---
+++ DataAccessLayer/Repositories/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace DataAccessLayer.Repositories
+{
+    public interface IRepository<TKey, T> where T : class
+    {
+        T Get(TKey id);
+
+        List<T> Get();
+
+        void Create(T entity);
+
+        bool Exists(Expression<Func<T, bool>> expression);
+
+        void SaveChanges();
+    }
+}
