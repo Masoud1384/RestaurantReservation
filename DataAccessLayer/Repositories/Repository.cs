5a3bf3ccfbbd246c0a16cdcca4a4d2672721d654
masoud <mwsoush.84@gmail.com>
2023-09-19 11:13:41 -0700

    Repositories methods has been implemented

--- DataAccessLayer/Repositories/Repository.cs
+++ DataAccessLayer/Repositories/Repository.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq.Expressions;
 
 namespace DataAccessLayer.Repositories
 {
-    internal class Repository
+    public class Repository<TKey, T> : IRepository<TKey, T> where T : class
     {
+        private readonly Context _context;
+
+        public Repository(Context context)
+        {
+            _context = context;
+        }
+
+        public T Get(TKey id)
+        {
+            return _context.Find<T>(id);
+        }
+
+        public List<T> Get()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public void Create(T entity)
+        {
+            _context.Add(entity);
+        }
+
+        public bool Exists(Expression<Func<T, bool>> expression)
+        {
+            return _context.Set<T>().Any(expression);
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+
     }
 }
--- DataAccessLayer/Repositories/UserRepository.cs
+++ DataAccessLayer/Repositories/UserRepository.cs
@@ -1,9 +1,23 @@
-﻿namespace DataAccessLayer.Repositories
+﻿using RestaurantReservation;
+using System.Linq.Expressions;
+
+namespace DataAccessLayer.Repositories
 {
-    public class UserRepository : IUserRepository ,
+    public class UserRepository : IUserRepository, Repository<int, User>
     {
+        public User FindUser(Expression<Func<User, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
 
+        public List<User> Ingredients(Expression<Func<User, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
 
-
+        public void Update(User recipeRating)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
