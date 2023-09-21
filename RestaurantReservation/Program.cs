b05aaca13248bac736b3da595e7524fb806b60ca
masoud <mwsoush.84@gmail.com>
2023-09-20 17:01:13 -0700

    DI container has been initialized

--- RestaurantReservation/Program.cs
+++ RestaurantReservation/Program.cs
@@ -1,3 +1,4 @@
+using BusinessLogicLayer;
 using DataAccessLayer;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
@@ -12,7 +13,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<Context>(
     options=>options.UseSqlServer(builder.Configuration.GetConnectionString("Connection")));
-
+Bootstrapper.ConfigureProjectServices();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
